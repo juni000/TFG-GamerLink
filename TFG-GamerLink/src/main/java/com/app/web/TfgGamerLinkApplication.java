bbd615bef9d58886f7c9942e99fe72c0015d34e1
juni000 <juanalbertojimenez84@gmail.com>
2025-03-20 15:16:02 +0100

    Primer commit
    Primer commit con las bases

---
+++ TFG-GamerLink/src/main/java/com/app/web/TfgGamerLinkApplication.java
@@ -0,0 +1,13 @@
+package com.app.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TfgGamerLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TfgGamerLinkApplication.class, args);
+	}
+
+}
